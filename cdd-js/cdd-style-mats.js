30e3363e243f57a313e8fe0f64b8685341082e14
aligulerworks <aligulerworks@gmail.com>
2024-05-13 17:53:41 +0300

    Graph Bar Design & Some Fixes

--- cdd-js/cdd-style-mats.js
+++ cdd-js/cdd-style-mats.js
@@ -2,5 +2,5 @@ const dropdownButton = document.querySelector(".cdd-dropdown-button");
 const dropdownOptions = document.querySelector(".cdd-dropdown-options");
 
 dropdownButton.addEventListener("click", function () {
-  dropdownOptions.classList.toggle("show-block"); // Toggle visibility using class
+  dropdownOptions.classList.toggle("show-block");
 });
